f538a1910675eaf1ec728bc44b71435e213873bf
macindex <machollanda@gmail.com>
2022-10-21 21:23:43 -0300

    produce

--- simulador-aluno/simulator/application/kafka/produce.go
+++ simulador-aluno/simulator/application/kafka/produce.go
@@ -2,22 +2,18 @@ package kafka
 
 import (
 	"encoding/json"
-	route2 "github.com/dropecosta/imersaofsfc2-simulator/application/route"
-	"github.com/dropecosta/imersaofsfc2-simulator/infra/kafka"
-	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
 	"os"
 	"time"
+
+	"github.com/codeedu/imersaofsfc2-simlulador/application/route"
+	"github.com/codeedu/imersaofsfc2-simlulador/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-// Produce is responsible to publish the positions of each request
-// Example of a json request:
-// {"clientId":"1","routeId":"1"}
-// {"clientId":"2","routeId":"2"}
-// {"clientId":"3","routeId":"3"}
-func Produce(msg *ckafka.Message) {
+func Produce(msg *ckafka.Message){
 	producer := kafka.NewKafkaProducer()
-	route := route2.NewRoute()
+	route := route.NewRoute()
 	json.Unmarshal(msg.Value, &route)
 	route.LoadPositions()
 	positions, err := route.ExportJsonPositions()
@@ -26,6 +22,6 @@ func Produce(msg *ckafka.Message) {
 	}
 	for _, p := range positions {
 		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
-		time.Sleep(time.Millisecond * 500)
+		time.Sleep(time.Microsecond * 500)
 	}
 }
